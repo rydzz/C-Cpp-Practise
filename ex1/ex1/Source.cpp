cdb89a6686c394bedb429964b9d8b510c62460d5
DRY <rydryzz@gmail.com>
2017-06-26 20:44:46 +0800

    n ge zhengshu weishu

--- ex1/ex1/Source.cpp
+++ ex1/ex1/Source.cpp
@@ -4,20 +4,11 @@ int main() {
 	int number = 0;
 	int count = 1;
 	scanf_s("%d", &number);
-	if (number >= 0 && number <= 9999) {
-
-		if ((number / 1000) != 0) {
+		while((number / 10)!=0) {
 			count++;
-			number = number % 1000;
+			number = number /10;
 		}
-		if (number / 100 != 0) {
-			count++;
-			number = number % 100;
-		}
-		if (number / 10 != 0) {
-			count++;
-			number = number % 10;
-		}
-	}
+		
 	printf("%d\n", count);
+
 }
